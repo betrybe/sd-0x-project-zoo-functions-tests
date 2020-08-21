5039008c2149e0fd1e5291a3a0ac947a103303cf
Gabriel Oliva <gfpoliva@gmail.com>
2020-08-21 01:11:27 -0300

    Refatora animalsByIds

--- test/animalsByIds.test.js
+++ test/animalsByIds.test.js
@@ -1,19 +1,16 @@
 const assert = require('assert');
 const zoo = require('../src/zoo');
 
-describe('animalsByIds', () => {
-  it('test', () => {
-    let actual, expected;
-
-    // sem parâmetros, retorna um array vazio
-    actual = zoo.animalsByIds();
-    expected = [];
-
+describe('Implemente a função animalsByIds', () => {
+  it('Caso receba nenhum parâmetro, necessário retornar um array vazio', () => {
+    const actual = zoo.animalsByIds();
+    const expected = [];
     assert.deepEqual(actual, expected);
+  });
 
-    // com um único id, retorna os animais com este id
-    actual = zoo.animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce');
-    expected = [{
+  it('Ao receber como parâmetro um único id, retorna os animais com este id', () => {
+    const actual = zoo.animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce');
+    const expected = [{
       id: '0938aa23-f153-4937-9f88-4858b24d6bce',
       name: 'lions',
       popularity: 4,
@@ -24,14 +21,14 @@ describe('animalsByIds', () => {
         { name: 'Faustino', sex: 'male', age: 7 },
         { name: 'Dee', sex: 'female', age: 14 }
       ]
-    }];
+    }]
 
     assert.deepEqual(actual, expected);
+  });
 
-    // com mais de um id, retorna os animais que têm um desses ids
-    actual = zoo.animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce',
-      'e8481c1d-42ea-4610-8e11-1752cfc05a46');
-    expected = [{
+  it('Ao receber mais de um id, retorna os animais que têm um desses ids', () => {
+    const actual = zoo.animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46');
+    const expected = [{
       id: '0938aa23-f153-4937-9f88-4858b24d6bce',
       name: 'lions',
       popularity: 4,
@@ -42,7 +39,8 @@ describe('animalsByIds', () => {
         { name: 'Faustino', sex: 'male', age: 7 },
         { name: 'Dee', sex: 'female', age: 14 }
       ]
-    }, {
+    },
+    {
       id: 'e8481c1d-42ea-4610-8e11-1752cfc05a46',
       name: 'tigers',
       popularity: 5,
